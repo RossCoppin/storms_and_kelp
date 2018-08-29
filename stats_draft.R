8fc781bfdfb629ce0e5c19b17a1081c96c4cf105
RossCoppin <coppinross@gmail.com>
2018-08-30 00:29:21 +0200

    2nd commit

--- stats_draft.R
+++ stats_draft.R
@@ -1,11 +1,21 @@
-library(vegan)
-library(ggplot2)
 library(tidyverse)
+library(vegan)
+library(gridExtra)
+library(lubridate)
+library(ggpubr)
+library(ggrepel)
 library(reshape2)
 
-df <- read.csv("data/stormcast_2017.csv", sep = ";") 
+morph_df <- read.csv("data/stormcast_2017.csv", sep = ";") 
+morph_df <- subset(morph_df, select = -c(1,3,9,10))
 
-df_long <- df %>%  
-  melt(value.name = "value", id.vars = c("ind","site", "date", "phase"), variable.name = "morph")
+morph_lng_df <- melt(data = morph_df, value.name = "measurement", id.vars = c("site", "phase"), variable.name = "morph")
+
+# Mean morphology per site
+
+morph_gather_mean <- morph_lng_df %>%
+  group_by(site, morph) %>%
+  summarise(mean = mean(measurement, na.rm = TRUE)) %>%
+  ungroup()
 
 
