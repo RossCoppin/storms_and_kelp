c7bce7b60509cbc086be3f14ff6c794c5b5ed560
RossCoppin <coppinross@gmail.com>
2018-08-30 00:29:36 +0200

    3rd commit

--- stats_draft.R
+++ stats_draft.R
@@ -1,21 +1,20 @@
 library(tidyverse)
 library(vegan)
-library(gridExtra)
 library(lubridate)
 library(ggpubr)
-library(ggrepel)
 library(reshape2)
 
-morph_df <- read.csv("data/stormcast_2017.csv", sep = ";") 
+morph_df <- read.csv("data/stormcast_2017.csv", sep = ";", as.is=T) 
 morph_df <- subset(morph_df, select = -c(1,3,9,10))
 
 morph_lng_df <- melt(data = morph_df, value.name = "measurement", id.vars = c("site", "phase"), variable.name = "morph")
 
-# Mean morphology per site
-
-morph_gather_mean <- morph_lng_df %>%
-  group_by(site, morph) %>%
-  summarise(mean = mean(measurement, na.rm = TRUE)) %>%
-  ungroup()
-
+ggplot(data = morph_lng_df, aes(x = site, y = measurement)) +
+  geom_boxplot() +
+  facet_wrap(~morph, scales = "free_x", strip.position = "bottom", ncol = 5) +
+  coord_flip() + 
+  theme(strip.background = element_blank(), strip.placement = "outside", text = element_text(size = 4.5)) +
+  theme_bw() +
+  xlab("") +
+  ylab("")
 
